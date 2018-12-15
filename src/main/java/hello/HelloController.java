0fcb429e4f2c8fd47070ff97247cb58509fd220d
Nicocovi <nico0205@hotmail.com>
2018-12-15 12:49:01 +0100

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "This is a sample microservice representing project: Masterarbeitssoftware-frontend";
     }
     
 }
