b5a45aa59a534dfae58072ad28cdc2f14d2692e7
ludwigachhammer <38220221+ludwigachhammer@users.noreply.github.com>
2019-05-06 20:44:55 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -140,9 +140,9 @@ node {
                                      $class          : 'UsernamePasswordMultiBinding',
                                      credentialsId   : '05487704-f456-43cb-96c3-72aaffdba62f',
                                      usernameVariable: 'CF_USERNAME',
-                                     passwordVariable: '"CF_PASSWORD"'
+                                     passwordVariable: 'CF_PASSWORD'
                              ]]) {
-                   bat 'cf login -a https://api.run.pivotal.io -u $CF_USERNAME -p \"${CF_PASSWORD}\" --skip-ssl-validation'
+                   bat 'cf login -a https://api.run.pivotal.io -u $CF_USERNAME -p \"$CF_PASSWORD\" --skip-ssl-validation'
                 bat 'cf target -o ead-tool -s development'
                 bat 'cf push '+NAME+' -f '+manifest+' --hostname '+NAME+' -p '+path
             }
