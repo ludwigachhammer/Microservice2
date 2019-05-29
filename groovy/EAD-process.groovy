8b6e367cb0f546b55b9141437ebd16898a907ebd
ludwigachhammer <38220221+ludwigachhammer@users.noreply.github.com>
2019-05-29 09:59:55 +0200

    Update EAD-process.groovy

--- groovy/EAD-process.groovy
+++ groovy/EAD-process.groovy
@@ -74,7 +74,7 @@ node {
     }
     */
 
-    dir("C:\Program Files (x86)\Jenkins\workspace\simplebuild") {
+    dir("C:/Program Files (x86)/Jenkins/workspace/simplebuild") {
         stage("Validating Config"){
             //TODO
             //Validate jira link in links.config
